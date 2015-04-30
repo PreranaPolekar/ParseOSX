5babb07c1932e1e0ef5f1490c83066eb6e2ed8be
Prerana <prerana.polekar28@gmail.com>
2015-04-30 16:53:43 +0530

    Object getting saved

--- MyParseDemo/AppDelegate.h
+++ MyParseDemo/AppDelegate.h
@@ -10,6 +10,7 @@
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
+- (IBAction)getData:(id)sender;
 
 @end
 
--- MyParseDemo/AppDelegate.m
+++ MyParseDemo/AppDelegate.m
@@ -7,6 +7,9 @@
 //
 
 #import "AppDelegate.h"
+#import <ParseOSX/ParseOSX.h>
+#import <OSAKit/OSAKit.h>
+
 
 @interface AppDelegate ()
 
@@ -17,10 +20,48 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    [Parse enableLocalDatastore];
+    
+    [Parse setApplicationId:@"Q9Oq2XmwoukyoqjrQ9wyD3DesC4k307OMBCTOtq8"
+                  clientKey:@"MnFVjeWENvhEYtWpT0r16vj8kD0JvyAriQ1Yv6Qa"];
+    [PFAnalytics trackAppOpenedWithLaunchOptions:nil];
+
+    //PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
+    //testObject[@"foo"] = @"bar";
+    //[testObject saveInBackground];
+    //NSLog(@"Object Saved");
+    
+    PFObject *gameScore = [PFObject objectWithClassName:@"GameScore"];
+    gameScore[@"score"] = @2288;
+    gameScore[@"playerName"] = @"Brett Lee";
+    gameScore[@"cheatMode"] = @YES;
+    
+    [gameScore saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (succeeded) {
+              // The object has been saved.
+            NSLog(@"Object saved");
+        } else {
+            // There was a problem, check error.description
+            NSLog(@"Error thrown, please check");
+        }
+    }];
+    
+    
 }
+
+
+
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
 
+- (IBAction)getData:(id)sender {
+    PFQuery *query = [PFQuery queryWithClassName:@"GameScore"];
+    [query getObjectInBackgroundWithId:@"Dhxtw2NqZV" block:^(PFObject *gameScore, NSError *error) {
+        // Do something with the returned PFObject in the gameScore variable.
+        NSLog(@"%@", gameScore);
+    }];
+    
+}
 @end
